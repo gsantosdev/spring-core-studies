db154db09a87b0005e3bf7742278baaa10126a45
santo <santosgustavo431@gmail.com>
2023-01-21 10:57:11 -0300

    Creating example of context's bean management

---
+++ src/main/java/main/Example1.java
@@ -0,0 +1,32 @@
+package main;
+
+import beans.Vehicle;
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Honda City");
+        System.out.println("Vehicle name from non-spring context is: " + vehicle.getName());
+
+        /*-----------------------------------------------------------------------------------*/
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle veh = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + veh.getName());
+
+        /*
+        We don't need to do any explicit casting while fetching a bean from context.
+        Spring is smart enough to look for a bean of the type you requested in its context.
+        if such a bean doesn't exist, Spring will throw an exception.
+        */
+        String hello = context.getBean(String.class);
+        System.out.println("String value from Spring context is: " + hello);
+        Integer num = context.getBean(Integer.class);
+        System.out.println("Integer value from Spring context is: " + num);
+
+    }
+}
