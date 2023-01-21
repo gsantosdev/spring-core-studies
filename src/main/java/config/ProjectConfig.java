3503bad56ebd4bf83eabdc276552c8437d20970f
santo <santosgustavo431@gmail.com>
2023-01-21 10:56:35 -0300

    Creating a configuration class example

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -1,11 +1,45 @@
 package config;
 
+import beans.Vehicle;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 /*
-
+    Spring @Configuration annotation is part of the spring core framework.
+    Spring Configuration annotation indicates that the class has @Bean definition methods.
+    So spring container can process the class and generate spring beans to be used in the application.
  */
 @Configuration
 public class ProjectConfig {
+
+    /*
+        @Bean Annotation, wich lets Spring know that it needs to call this method when it initializes its context and
+        adds the return value to the context
+     */
+    @Bean
+    Vehicle vehicle() {
+        var veh = new Vehicle();
+        veh.setName("Audi 8");
+        return veh;
+    }
+
+    /*
+    The method names usually follow verbs notation. But for methods
+    which we will use to create beans, can use nouns as names.
+    This will be a good practise as the method names will become
+    beans names as well in the context.
+     */
+
+    @Bean
+    String hello() {
+        return "Hello World";
+    }
+
+    @Bean
+    Integer number() {
+        return 16;
+    }
 }
+
+
