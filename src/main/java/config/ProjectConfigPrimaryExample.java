fbef7989f3c32070e92a8dd35dbfa6a9cb3dd7b4
santo <santosgustavo431@gmail.com>
2023-01-25 12:46:15 -0300

    Creating project config for @primary example

---
+++ src/main/java/config/ProjectConfigPrimaryExample.java
@@ -0,0 +1,34 @@
+package config;
+
+import beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ProjectConfigPrimaryExample {
+
+    /*ONLY ONE PRIMARY BEAN OF ONE TYPE CAN BE DEFINED*/
+
+    @Bean(name = "audiVehicle")
+    Vehicle vehicle1() {
+        var veh = new Vehicle();
+        veh.setName("Audi");
+        return veh;
+    }
+
+    @Bean(value = "hondaVehicle")
+    Vehicle vehicle2() {
+        var veh = new Vehicle();
+        veh.setName("Honda");
+        return veh;
+    }
+
+    @Primary
+    @Bean("ferrariVehicle")
+    Vehicle vehicle3() {
+        var veh = new Vehicle();
+        veh.setName("Ferrari");
+        return veh;
+    }
+}
