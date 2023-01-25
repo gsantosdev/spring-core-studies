4814ba3c03a28f6f92996cd9bf9d62e810d780bb
santo <santosgustavo431@gmail.com>
2023-01-25 12:46:46 -0300

    Understanding @Primary annotation when getting a Bean

---
+++ src/main/java/main/PrimaryAnottationExample.java
@@ -0,0 +1,21 @@
+package main;
+
+import beans.Vehicle;
+import config.ProjectConfig;
+import config.ProjectConfigPrimaryExample;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class PrimaryAnottationExample {
+
+    public static void main(String[] args) {
+
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfigPrimaryExample.class);
+
+        /*A default bean was defined, so it will not throw exception when you don't pass the bean name */
+
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Primary Vehicle name from Spring context is: " + vehicle.getName());
+
+    }
+}
